dd25a1f14d8f7cc86791946e3261f680510be120
mehmetmustafakilic <96634748+mehmetmustafakilic@users.noreply.github.com>
2021-12-28 11:16:31 +0300

    Update question1.bash

--- question1.bash
+++ question1.bash
@@ -1,14 +1,46 @@
-h = eval(input("Enter diamond's height: "))
-if (h % 2 ==0 ):
-        print("Value not perfect")
-elif:        
-    for i in range(1, h):
-        for j in range(1, i+1):
-                print("*", end="")
-    print()
-        
-else:
-    for x in range(h):
-        print(" " * (h - x), "*" * (2*x + 1))
-    for x in range(h - 2, -1, -1):
-        print(" " * (h - x), "*" * (2*x + 1))
+echo "Enter an integer"
+read num
+if[$num %2==1]
+then
+for (( i=1;i<=$num ;i++))
+do
+   for (( j=$num;j>=i;j-- ))
+   do
+   echo -n " "
+   done
+   for (( c=1;c<=i;c++ ))
+   do
+   echo -n " *"
+   sum=`expr $sum + 1`
+   done
+echo ""
+done
+d_max=`expr $num - 1`
+for (( i=$d_max;i>=1;i--))
+do
+   for (( j=i;j<=$d_max;j++ ))
+   do
+   if [ $j -eq $d_max ]
+   then
+   echo -n " "
+   fi
+   echo -n " "
+   done
+   for (( c=1;c<=i;c++ ))
+   do
+   echo -n " *"
+   sum=`expr $sum + 1`
+   done
+echo ""
+done
+else
+
+for((i=$num; i>=1; i--))
+do
+  for((j=1; j<=$i; j++))
+  do
+    echo -n "* "
+  done
+  echo
+done
+fi
